d80352358be9b1bf7641bfebf477a220c41279d7
watanabe erika <erika.watanabe@rakus-partners.co.jp>
2022-09-15 17:17:45 +0900

    掲示板システム

---
+++ src/test/java/com/example/demo/ExBbsPraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExBbsPraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
